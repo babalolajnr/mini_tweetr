97d363c6e1014024a56b0953f412a739aa884181
Abdulqudduus Babalola <babalolajnr@gmail.com>
2022-03-11 14:59:16 +0100

    add save tweet method

--- tweet/views.py
+++ tweet/views.py
@@ -1,8 +1,18 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+
+from tweet.forms import TweetForm
 
 # Create your views here.
 
 
 def index(request):
-    context = {'hello': 'world'}
-    return render(request, 'tweet/main.html', context)
+    return render(request, 'tweet/main.html')
+
+
+def save_tweet(request):
+    if request.method == 'POST':
+        form = TweetForm(request.POST)
+
+        if form.is_valid():
+            return HttpResponse(request.body)
