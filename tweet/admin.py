44f4099ac64581ebd96b14cebecca4ce9c10ed13
Abdulqudduus Babalola <babalolajnr@gmail.com>
2022-03-13 07:58:55 +0100

    register user and userAdmin classes

--- tweet/admin.py
+++ tweet/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
+from user.models import User
+from django.contrib.auth.admin import UserAdmin
 
 from tweet.models import Retweet, Tweet
 
 # Register your models here.
 admin.site.register(Tweet)
 admin.site.register(Retweet)
+admin.site.register(User, UserAdmin)
