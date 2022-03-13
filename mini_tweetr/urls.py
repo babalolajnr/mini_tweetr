3b789a35587501682f1487a5103c4c3b7dae5ab0
Abdulqudduus Babalola <babalolajnr@gmail.com>
2022-03-13 07:56:20 +0100

    add user urls

--- mini_tweetr/urls.py
+++ mini_tweetr/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('tweet/', include('tweet.urls'))
+    path('tweet/', include('tweet.urls')),
+    path('user/', include('user.urls'))
 ]
