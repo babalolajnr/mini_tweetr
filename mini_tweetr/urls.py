d7873d9b8b37206657dd68244daae7155e0136aa
Abdulqudduus Babalola <babalolajnr@gmail.com>
2022-03-13 15:15:54 +0100

    add login url

--- mini_tweetr/urls.py
+++ mini_tweetr/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from user.views import loginPage
+
 urlpatterns = [
+    path('login/', loginPage),
     path('admin/', admin.site.urls),
     path('tweet/', include('tweet.urls')),
-    path('user/', include('user.urls'))
 ]
