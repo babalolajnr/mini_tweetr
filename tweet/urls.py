a22bd3dd17b196e9363c9d56a359fa14b998e698
Abdulqudduus Babalola <babalolajnr@gmail.com>
2022-03-16 16:27:24 +0100

    remove greet route

--- tweet/urls.py
+++ tweet/urls.py
@@ -4,12 +4,7 @@ from django.urls import path
 from tweet.views import index, save_tweet
 
 
-def greet(request):
-    return HttpResponse('Hello Boss man')
-
-
 urlpatterns = [
-    path('hello/', greet),
-    path('', index),
+    path('', index, name='home'),
     path('save_tweet', save_tweet, name='save_tweet'),
 ]
