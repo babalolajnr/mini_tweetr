52aaf16d8550f33fe928e524ef0fb8c300d0f898
Abdulqudduus Babalola <babalolajnr@gmail.com>
2022-03-22 13:30:56 +0100

    add ability to switch tab view based on tab clicked on

--- static/js/profile.js
+++ static/js/profile.js
@@ -3,6 +3,18 @@ const tweetsAndRepliesTab = document.getElementById('tweets-and-replies-tab');
 const likesTab = document.getElementById('likes-tab');
 const mediaTab = document.getElementById('media-tab');
 
+const tweetsTabView = document.getElementById('tweets-tab-view');
+const tweetsAndRepliesTabView = document.getElementById('tweets-and-replies-tab-view')
+const likesTabView = document.getElementById('likes-tab-view')
+const mediaTabView = document.getElementById('media-tab-view')
+
+const relations = {
+    'tweets-tab': [tweetsTab, tweetsTabView],
+    'tweets-and-replies-tab': [tweetsAndRepliesTab, tweetsAndRepliesTabView],
+    'likes-tab': [likesTab, likesTabView],
+    'media-tab': [mediaTab, mediaTabView]
+}
+
 const tabs = [tweetsTab, likesTab, mediaTab, tweetsAndRepliesTab];
 
 const parser = new DOMParser();
@@ -12,8 +24,23 @@ const activeTabIndicatorString = "<div class='w-16 h-1 bg-yellow-400 rounded-ful
 // which his the node we need to append to the parent div
 const activeTabIndicator = parser.parseFromString(activeTabIndicatorString, 'text/html').body.firstChild
 
+// On page load set tweetsTab as active
+tweetsTab.appendChild(activeTabIndicator)
+
 tabs.forEach(element => {
     element.onclick = () => {
         element.appendChild(activeTabIndicator)
+
+        // Iterate through relation keys and find one that matches
+        // the element clicked on's id
+        Object.keys(relations).forEach((i) => {
+            if (i == element.id) {
+                const tabView = relations[i][1]
+                tabView.classList.remove('hidden')
+            } else {
+                const tabView = relations[i][1]
+                tabView.classList.add('hidden')
+            }
+        })
     }
 });
