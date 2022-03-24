0aae3877a9de69b8a7a1559682afc9225c0e2f2c
Abdulqudduus Babalola <babalolajnr@gmail.com>
2022-03-24 15:08:46 +0100

    Close modal if anywhere is clicked outside the modal content

--- static/js/edit-profile-modal.js
+++ static/js/edit-profile-modal.js
@@ -1,6 +1,18 @@
 const close = document.getElementById('close')
+const modalContent = document.getElementById('modal-content')
 
 close.onclick = () => {
     editProfileModal.classList.remove('flex')
     editProfileModal.classList.add('hidden')
+}
+
+const modalContentChildren = modalContent.querySelectorAll('*')
+const modalContentChildrenArray = Array.from(modalContentChildren)
+
+// Close modal if anywhere is clicked outside the modal content
+document.getElementById('edit-profile-modal').onclick = e => {
+    if (e.target != modalContent && !modalContentChildrenArray.includes(e.target)) {
+        editProfileModal.classList.remove('flex')
+        editProfileModal.classList.add('hidden')
+    }
 }
