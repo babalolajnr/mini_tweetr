23f162d49249b09b76f9a76ee534a536190ed23e
Abdulqudduus Babalola <babalolajnr@gmail.com>
2022-03-26 11:29:19 +0100

    add profile form

--- user/forms.py
+++ user/forms.py
@@ -4,3 +4,10 @@ from django import forms
 class LoginForm(forms.Form):
     email = forms.EmailField()
     password = forms.CharField()
+
+class ProfileForm(forms.Form):
+    first_name = forms.CharField(max_length=25)
+    last_name = forms.CharField(max_length=25)
+    bio = forms.CharField(max_length=160, required=False)
+    location = forms.CharField(max_length=160, required=False)
+    website = forms.CharField(max_length=100, required=False)
