863fb8fc5080765a3d61e1eb1da564b02d046c5e
Abdulqudduus Babalola <babalolajnr@gmail.com>
2022-03-26 11:24:01 +0100

    add save profile url

--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,11 @@
 from django.http import HttpResponse
 from django.urls import path
 
-from .views import profile, login
+from .views import profile, login, save_profile
 
 
 urlpatterns = [
     path("login/", login, name="login"),
     path('profile', profile, name='profile'),
+    path('save_profile', save_profile, name='save_profile'),
 ]
