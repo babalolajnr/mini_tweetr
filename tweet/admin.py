24a04a5080a796d63ceb69e568353bf8c3e9ccf1
Abdulqudduus Babalola <babalolajnr@gmail.com>
2022-04-02 04:33:31 +0100

    remove Retweet

--- tweet/admin.py
+++ tweet/admin.py
@@ -2,9 +2,8 @@ from django.contrib import admin
 from user.models import User
 from django.contrib.auth.admin import UserAdmin
 
-from tweet.models import Retweet, Tweet
+from tweet.models import Tweet
 
 # Register your models here.
 admin.site.register(Tweet)
-admin.site.register(Retweet)
 admin.site.register(User, UserAdmin)
