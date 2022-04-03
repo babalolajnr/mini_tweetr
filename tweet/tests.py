888b5914cdf7a80909c951f5a19d73a956ea3f95
Abdulqudduus Babalola <babalolajnr@gmail.com>
2022-04-03 22:28:58 +0100

    add TweetTestCase

--- tweet/tests.py
+++ tweet/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from django.urls import reverse
+
+from user.models import User
 
 # Create your tests here.
+class TweetTestCase(TestCase):
+    def setUp(self) -> None:
+        self.user = {"email": "user@gmail.com", "password": "password"}
+        User.objects.create_user(**self.user)
+        
+    def test_index(self):
+        self.client.login(**self.user)
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'tweet/home.html')
+        # self.assertDictContainsSubset({'tweets': tweets}, response.context)
+
+    def test_tweet_can_be_created(self):
+        pass
