36c17bfcf338523afb0663d88bb3aa548611f8cd
Abdulqudduus Babalola <babalolajnr@gmail.com>
2022-04-03 22:19:02 +0100

    add test_user_can_save_profile

--- user/tests.py
+++ user/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 from django.urls import reverse
+from faker import Faker
+from user.factory import UserFactory
 
 from user.models import User
 
@@ -7,13 +9,25 @@ from user.models import User
 class UserTestCase(TestCase):
     def setUp(self) -> None:
         self.user = {"email": "user@gmail.com", "password": "password"}
+        self.faker = Faker()
         User.objects.create_user(**self.user)
 
-    def test_user_profile(self):
+    def test_user_can_get_profile_view(self):
         self.client.login(**self.user)
         response = self.client.get(reverse("profile"), self.user)
         self.assertEqual(response.status_code, 200)
 
-    def test_user_login(self):
+    def test_user_can_login(self):
         response = self.client.post(reverse("login"), self.user)
         self.assertEqual(response.status_code, 302)
+
+    def test_user_can_save_profile(self):
+        self.client.login(**self.user)
+        response = self.client.post(
+            reverse("save_profile"),
+            {
+                "bio": self.faker.text(),
+                "location": self.faker.city(),
+            },
+        )
+        self.assertEqual(response.status_code, 200)
