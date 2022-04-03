2687170c52ce2df78d32e647cfa8a7ae0317e1c8
Abdulqudduus Babalola <babalolajnr@gmail.com>
2022-04-03 05:35:22 +0100

    import textarea

--- jstoolchain/js/main.js
+++ jstoolchain/js/main.js
@@ -1,3 +1,6 @@
+import './textarea'
+import './like-tweet'
+
 const body = document.getElementsByTagName('body')[0]
 const view = body.dataset.view
 const popup = document.getElementById('popup')
@@ -9,20 +12,5 @@ if (popup != null) {
 if (view === 'profile') {
     require('./profile')
     require('./edit-profile-modal')
-}
-
-// Resize textarea on input dynamically
-const tx = document.getElementsByTagName("textarea");
-for (let i = 0; i < tx.length; i++) {
-    tx[i].setAttribute(
-        "style",
-        "height:" + tx[i].scrollHeight + "px;overflow-y:hidden;"
-    );
-    tx[i].addEventListener("input", OnInput, false);
-}
-
-function OnInput() {
-    this.style.height = "auto";
-    this.style.height = this.scrollHeight + "px";
 }
 
