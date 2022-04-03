c3dfd60fe792a949703f85d64834768c9ea6442a
Abdulqudduus Babalola <babalolajnr@gmail.com>
2022-04-03 15:47:48 +0100

    add test

--- user/tests.py
+++ user/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.urls import reverse
+
+from user.models import User
 
 # Create your tests here.
+class UserTestCase(TestCase):
+    def setUp(self) -> None:
+        self.user = {"email": "user@gmail.com", "password": "password"}
+        User.objects.create_user(**self.user)
+
+    def test_user_profile(self):
+        self.client.login(**self.user)
+        response = self.client.get(reverse("profile"), self.user)
+        self.assertEqual(response.status_code, 200)
