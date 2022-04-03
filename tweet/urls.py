0b44a82a9d684d32624b1ae5cb73f3b94af3332d
Abdulqudduus Babalola <babalolajnr@gmail.com>
2022-04-03 12:13:33 +0100

    add unlike tweet url

--- tweet/urls.py
+++ tweet/urls.py
@@ -1,11 +1,12 @@
 from django.http import HttpResponse
 from django.urls import path
 
-from tweet.views import index, like_tweet, save_tweet
+from tweet.views import index, like_tweet, save_tweet, unlike_tweet
 
 
 urlpatterns = [
     path('', index, name='home'),
     path('save_tweet', save_tweet, name='save_tweet'),
     path('like_tweet/<int:tweet_id>/', like_tweet, name='like_tweet'),
+    path('unlike_tweet/<int:tweet_id>/', unlike_tweet, name='unlike_tweet'),
 ]
