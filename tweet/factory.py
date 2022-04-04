8dc37e9ab53a896a87287c5416c7b4d7670a5430
Abdulqudduus Babalola <babalolajnr@gmail.com>
2022-04-04 16:34:23 +0100

    add user subfactory

--- tweet/factory.py
+++ tweet/factory.py
@@ -1,10 +1,11 @@
 import factory
 from factory.django import DjangoModelFactory
 from tweet.models import Tweet
+from user.factory import UserFactory
 
 
 class TweetFactory(DjangoModelFactory):
     class Meta:
         model = Tweet
 
-    
+    user = factory.SubFactory(UserFactory)
