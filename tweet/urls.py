da20dd3423824d18d3cc3a1d22df2677ecdfa5bc
Abdulqudduus <babalolajnr@gmail.com>
2022-04-17 17:26:57 +0100

    add retweet url

--- tweet/urls.py
+++ tweet/urls.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.urls import path
 
-from tweet.views import index, like_tweet, save_tweet, unlike_tweet
+from tweet.views import index, like_tweet, retweet, save_tweet, unlike_tweet
 
 
 urlpatterns = [
@@ -9,4 +9,5 @@ urlpatterns = [
     path('save_tweet', save_tweet, name='save_tweet'),
     path('like_tweet/<int:tweet_id>/', like_tweet, name='like_tweet'),
     path('unlike_tweet/<int:tweet_id>/', unlike_tweet, name='unlike_tweet'),
+    path('retweet/<int:tweet_id>/', retweet, name='retweet'),
 ]
