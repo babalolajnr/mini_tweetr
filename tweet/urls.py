293f40993b3c884b1a97a91238878f7766415a40
Abdulqudduus <babalolajnr@gmail.com>
2022-04-18 07:05:17 +0100

    add unretweet url

--- tweet/urls.py
+++ tweet/urls.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.urls import path
 
-from tweet.views import index, like_tweet, retweet, save_tweet, unlike_tweet
+from tweet.views import index, like_tweet, retweet, save_tweet, unlike_tweet, unretweet
 
 
 urlpatterns = [
@@ -10,4 +10,5 @@ urlpatterns = [
     path('like_tweet/<int:tweet_id>/', like_tweet, name='like_tweet'),
     path('unlike_tweet/<int:tweet_id>/', unlike_tweet, name='unlike_tweet'),
     path('retweet/<int:tweet_id>/', retweet, name='retweet'),
+    path('unretweet/<int:tweet_id>/', unretweet, name='unretweet'),
 ]
